c11b4c5f16974753ef80de6cfc20ba2c833e200e
Piyush Shakya <piyushshakya112@gmail.com>
2025-09-17 14:46:54 +0530

    check

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const serverless = require('serverless-http');
 const app = require('./app');
 
+app.get("/", (req, res) => {
+    res.send("✅ Backend is live and connected to MongoDB");
+});
+
+
 module.exports = serverless(app);
